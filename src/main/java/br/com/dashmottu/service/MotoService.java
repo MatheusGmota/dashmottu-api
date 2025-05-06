962a39d4772f7c76a1e00d18d4c5a0e55231883a
MatheusGmota <livematgreg@gmail.com>
2025-05-06 18:50:03 -0300

    fix: Corrigindo erro de injeção de dependência

--- src/main/java/br/com/dashmottu/service/MotoService.java
+++ src/main/java/br/com/dashmottu/service/MotoService.java
@@ -3,9 +3,11 @@ package br.com.dashmottu.service;
 import br.com.dashmottu.model.entities.Moto;
 import br.com.dashmottu.repository.MotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MotoService {
 
     @Autowired
