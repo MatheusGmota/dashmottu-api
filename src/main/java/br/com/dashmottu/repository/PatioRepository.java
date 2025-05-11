fd790e2e161e8059c38dd43c669cf23d10f8226c
MatheusGmota <livematgreg@gmail.com>
2025-05-11 19:22:32 -0300

    changes: corrigindo erro na anotacao da classe

--- src/main/java/br/com/dashmottu/repository/PatioRepository.java
+++ src/main/java/br/com/dashmottu/repository/PatioRepository.java
@@ -2,8 +2,8 @@ package br.com.dashmottu.repository;
 
 import br.com.dashmottu.model.entities.Patio;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
-@Service
+@Repository
 public interface PatioRepository extends JpaRepository<Patio, Long> {
 }
