84ffbbb57e9219eda2463f02d0379cf0c5603c2f
MatheusGmota <livematgreg@gmail.com>
2025-05-11 19:22:06 -0300

    changes: colocando anotacao @Repository

--- src/main/java/br/com/dashmottu/repository/EnderecoRepository.java
+++ src/main/java/br/com/dashmottu/repository/EnderecoRepository.java
@@ -2,6 +2,8 @@ package br.com.dashmottu.repository;
 
 import br.com.dashmottu.model.entities.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 }
