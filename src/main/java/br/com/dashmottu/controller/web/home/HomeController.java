a8fd6cc124a223dff87a9abe77ab5bbca233e489
MatheusGmota <livematgreg@gmail.com>
2025-09-12 20:20:33 -0300

    fix: alt rota

--- src/main/java/br/com/dashmottu/controller/web/home/HomeController.java
+++ src/main/java/br/com/dashmottu/controller/web/home/HomeController.java
@@ -3,14 +3,16 @@ package br.com.dashmottu.controller.web.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.security.Principal;
 
 @Controller
-@RequestMapping()
+@RequestMapping("/")
+@EnableWebMvc
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String index(Principal principal) {
         return "home";
     }
