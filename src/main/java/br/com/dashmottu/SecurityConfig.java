1b811a2c6f9f863557c5aefee7d380ddb9c5a4a8
MatheusGmota <livematgreg@gmail.com>
2025-09-12 20:22:27 -0300

    Configurado rotas sem permissao

---
+++ src/main/java/br/com/dashmottu/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.com.dashmottu;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/").permitAll();
+                            authorizeConfig.requestMatchers("/api/moto").permitAll();
+                            authorizeConfig.requestMatchers("/api/patio").permitAll();
+                            authorizeConfig.requestMatchers("/logout").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        }
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
