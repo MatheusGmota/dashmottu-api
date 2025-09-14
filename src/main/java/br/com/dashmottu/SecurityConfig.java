c0be3f9fdd3b0a201e1235cdc0c0b61e70692cc7
MatheusGmota <livematgreg@gmail.com>
2025-09-14 12:24:50 -0300

    desabilitado csrf para api restful

--- src/main/java/br/com/dashmottu/SecurityConfig.java
+++ src/main/java/br/com/dashmottu/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.dashmottu;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,11 +14,12 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
                 .authorizeHttpRequests(
                         authorizeConfig -> {
-                            authorizeConfig.requestMatchers("/").permitAll();
-                            authorizeConfig.requestMatchers("/api/moto").permitAll();
-                            authorizeConfig.requestMatchers("/api/patio").permitAll();
+                            authorizeConfig.requestMatchers("/" ).permitAll();
+                            authorizeConfig.requestMatchers("/api/moto/**").permitAll();
+                            authorizeConfig.requestMatchers("/api/patio/**").permitAll();
                             authorizeConfig.requestMatchers("/logout").permitAll();
                             authorizeConfig.anyRequest().authenticated();
                         }
